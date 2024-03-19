7caf53e689637a98bafda8c870c418d08f23161a
Rocio EF FundacionJ <rocio.encinas@fundacion-jala.org>
2024-03-19 01:58:47 -0400

    RabinKarp example

---
+++ src/stringMatching/RabinKarp.java
@@ -0,0 +1,52 @@
+package stringMatching;
+
+// Rabin-Karp algorithm in Java
+
+public class RabinKarp {
+    public final static int d = 10;//d = base
+
+    //Pseudo code
+    public static void search(String pattern, String txt, int q) {
+        int m = pattern.length();
+        int n = txt.length();
+        int i, j;
+        int p = 0;
+        int t = 0;
+        int h = 1;
+
+        for (i = 0; i < m - 1; i++)
+            h = (h * d) % q;
+
+        // Hash calculation
+        for (i = 0; i < m; i++) {
+            p = (d * p + pattern.charAt(i)) % q;
+            t = (d * t + txt.charAt(i)) % q;
+        }
+
+        // Match hash with original txt
+        for (i = 0; i <= n - m; i++) {
+            if (p == t) {
+                for (j = 0; j < m; j++) {
+                    if (txt.charAt(i + j) != pattern.charAt(j))
+                        break;
+                }
+
+                if (j == m)
+                    System.out.printf("Pattern is found at index: %d ", i);
+            }
+
+            if (i < n - m) {
+                t = (d * (t - txt.charAt(i) * h) + txt.charAt(i + m)) % q;
+                if (t < 0)
+                    t = (t + q);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String txt = "APABCAXS";
+        String pattern = "ABC";
+        int q = 31;
+        search(pattern, txt, q);
+    }
+}
